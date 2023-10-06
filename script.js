8a1e2c280b0e8ce96932c3540e4aeebc88949371
Felipe Cristian <fc.fullstackdeveloper@gmail.com>
2023-10-06 10:00:48 -0300

    mudando o lado de entrada do menu responsivo.

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const burguer = document.querySelector('#burguer')
 burguer.addEventListener('click', openMenu)
 
 function openMenu() {
-    if (menu.style.left == '800px') {
-        menu.style.left = '0px'
+    if (menu.style.right == '-400px') {
+        menu.style.right = '0px'
     } else{
-        menu.style.left = '800px'
+        menu.style.right = '-400px'
     }
 }
