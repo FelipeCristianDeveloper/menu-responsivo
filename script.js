deaafac7682e1dc4e940a0f03307ad7e6d7f5833
Felipe Cristian <fc.fullstackdeveloper@gmail.com>
2023-10-06 09:51:54 -0300

    adicionando transição no menu

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ const burguer = document.querySelector('#burguer')
 burguer.addEventListener('click', openMenu)
 
 function openMenu() {
-    if (menu.style.display == 'none') {
-        menu.style.display = 'block'
+    if (menu.style.left == '800px') {
+        menu.style.left = '0px'
     } else{
-        menu.style.display = 'none'
+        menu.style.left = '800px'
     }
 }
